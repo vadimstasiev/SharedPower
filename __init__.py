a3064f2932410fd9390f5112b56897a1c1669c3a
Vadim Stasiev <vadim444@hotmail.com>
2019-12-24 01:41:59 +0000

    Deleted obsolete code, sorted code out and modified the error messaging to use buffer

--- __init__.py
+++ __init__.py
@@ -22,6 +22,7 @@ from Classes.UserAccounts import UserClass
 
 class guiInterface:
     def __init__(self):
+        self.buffered_user_errors = []
         self.user_account = UserClass()
         self.log_in_gui()
 
@@ -69,31 +70,19 @@ class guiInterface:
         self.root.title("Shared Power - Register New User")
         self.root.resizable(width=False, height=False)
         menubar = Menu(self.root)
+        options = Menu(menubar, tearoff=0)
 
-        menubar.add_command(label="Quit", command=self.root.quit)
-
+        options.add_command(label="Go Back", command=self.log_in_gui)
+        options.add_command(label="Quit", command=self.root.quit)
+        menubar.add_cascade(label="Options", menu=options)
         self.root.config(menu=menubar)
 
-        __labelframe1 = LabelFrame(self.root, text="Register User")
-        __labelframe1.grid(ipadx=50, ipady=30, padx=5, pady=5)
-        __inputPanel = PanedWindow(__labelframe1, orient=HORIZONTAL)
+        self._label_frame_register = LabelFrame(
+            self.root, text="Register User")
+        self._label_frame_register.grid(ipadx=50, ipady=30, padx=5, pady=5)
+        __inputPanel = PanedWindow(
+            self._label_frame_register, orient=HORIZONTAL)
         __inputPanel.grid(row=0, column=1, padx=50, pady=20)
-
-        labels_display = [
-            ("First Name", 0),
-            ("Surname", 1),
-            ("Phone Number", 2),
-            ("Date of Birth", 3),
-            ("Post Code", 4),
-            ("Home Address", 5),
-            ("Email", 6),
-            ("Password", 7),
-            ("Confirm Password", 8),
-            ("User Type", 9)
-        ]
-        for val, label_display in enumerate(labels_display):
-            Label(__inputPanel, text=label_display[0], padx=20, pady=3).grid(
-                row=val, sticky=W)
 
         self.first_name_input = StringVar()
         self.surname_input = StringVar()
@@ -104,21 +93,27 @@ class guiInterface:
         self.password_input = StringVar()
         self.confirm_password_input = StringVar()
 
-        entry_list = [
-            [self.first_name_input, 0],
-            [self.surname_input, 1],
-            [self.phone_number_input, 2],
-            [None, 3],  # skip date of birth, it has a different input
-            [self.post_code_input, 4],
-            [self.home_address_input, 5],
-            [self.email_input, 6],
-            [self.password_input, 7],
-            [self.confirm_password_input, 8]
+        new_labels_display = [
+            ("First Name", self.first_name_input),
+            ("Surname", self.surname_input),
+            ("Phone Number", self.phone_number_input),
+            ("Date of Birth", None),
+            ("Post Code", self.post_code_input),
+            ("Home Address", self.home_address_input),
+            ("Email", self.email_input),
+            ("Password", self.password_input),
+            ("Confirm Password", self.confirm_password_input),
+            ("User Type", None)
         ]
-        for val, entry in enumerate(entry_list):
-            if entry[0] != None:
-                entry[0] = Entry(__inputPanel, width=25)
-                entry[0].grid(row=val, column=1, columnspan=2, sticky=W)
+
+        for __line in new_labels_display:
+            label_display, var = __line
+            __index = new_labels_display.index(__line)
+            __label = Label(__inputPanel, text=label_display, padx=20, pady=3)
+            __label.grid(row=__index, sticky=W)
+            if var != None:
+                __entry = Entry(__inputPanel, width=25, textvariable=var)
+                __entry.grid(row=__index, column=1, columnspan=2, sticky=W)
 
         self.date_of_birth_entry = DateEntry(
             __inputPanel, width=22, background='darkblue', foreground='white', borderwidth=2)
@@ -134,12 +129,24 @@ class guiInterface:
         Radiobutton(__inputPanel, text="Tool Owner", variable=self.user_type,
                     value=2).grid(row=9, column=2, sticky=W)
 
-        submit_button = Button(__labelframe1, text="Register", command=self.process_register_new_user).grid(
+        submit_button = Button(self._label_frame_register, text="Register", command=self.process_register_new_user).grid(
             column=2, ipadx=10, ipady=5)
         self.textvar = StringVar()
+
         self.error_message_output = Label(
-            __labelframe1, textvariable=self.textvar, fg="#ff0000").grid(column=1)
+            self._label_frame_register, textvariable=self.textvar, fg="#ff0000")
+        self.error_message_output.grid(column=1)
+
         self.root.mainloop()
+
+    def register_user_gui_output_errors(self):
+        __buffered_errors = self.get_buffered_user_errors()
+        for __line in __buffered_errors:
+            __index = __buffered_errors.index(__line) + 99
+            # the number 99 is to make sure that it always stays at the bottom
+            __label = Label(self._label_frame_register,
+                            text=__line, fg="#ff0000")
+            __label.grid(row=__index, column=1)
 
     def tool_user_options_gui(self):
         self.reset_root()
@@ -176,32 +183,31 @@ class guiInterface:
                 if (__account_user_type == "Tool_User"):
                     self.tool_user_options_gui()
                 elif (__account_user_type == "Tool_Owner"):
-                    self.tool_user_options_gui()  # change to tool owner once that gets created
+                    self.tool_user_options_gui()  # change to tool owner once that gets added
                 else:
                     self.textvar.set("Database Error - Type of User Unknown")
             else:
                 self.textvar.set("ERROR - Wrong Password")
 
-    def process_register_new_user(self):
-        __password = self.get_hashed_password()
-        if(self.password_input.get() == self.confirm_password_input.get()):
-            self.textvar.set("")
-            self.user_account.register(
-                UserClass.generate_unique_ID(),
-                self.first_name_input.get(),
-                self.surname_input.get(),
-                self.date_of_birth_input,
-                self.phone_number_input.get(),
-                self.home_address_input.get(),
-                self.post_code_input.get(),
-                self.email_input.get(),
-                __password,
-                0,                                           # Outstanding Balance
-                "Tool_User" if self.user_type.get() == 1 else "Tool_Owner"
-            )
-            self.log_in_gui()
-        else:
-            self.textvar.set("ERROR - Passwords do not match")
+    def process_register_new_user(self):  # TODO
+        __valid_email_address = self.get_valid_email_address()
+        __password_hash = self.get_valid_hashed_password()
+        self.textvar.set("")
+        self.user_account.register(
+            UserClass.generate_unique_ID(),
+            str(self.first_name_input.get()),
+            str(self.surname_input.get()),
+            self.date_of_birth_input,
+            int(self.phone_number_input.get()),
+            str(self.home_address_input.get()),
+            str(self.post_code_input.get()),
+            __valid_email_address,
+            __password_hash,
+            0,                                           # Outstanding Balance
+            "Tool_User" if self.user_type.get() == 1 else "Tool_Owner"
+        )
+        self.register_user_gui_output_errors()  # TODO needs more work
+        self.log_in_gui()
 
     def reset_root(self):
         try:
@@ -213,6 +219,9 @@ class guiInterface:
 
     def contact_admin(self):
         print("Contact Admin")
+        # IDK what to do here, do something fancy lol
+        # Can add message inbox to admin's personal db for that stuff, logs, etc
+        # Can also create an interface for an admin account
 
     def menu_tool_owner_account(self):
         print("1: Register tool")
@@ -237,22 +246,7 @@ class guiInterface:
         }[__option]()
 
     def menu_default_user_account(self):
-        print("1: Search for tools")
-        print("2: View current orders")
-        print("3: View Purchase History")
-        print("4: View next Invoice")
-        print("5: Log Out")
-
-        self.print_select_your_option()
-
-        __option = self.get_valid_option(1, 5)
-        __switch = {
-            1: self.menu_search_for_tools,
-            2: self.menu_view_current_orders,
-            3: self.menu_view_purchase_history,
-            4: self.menu_view_next_invoice,
-            5: self.escape
-        }[__option]()
+        pass    # done this: self.tool_user_options_gui
 
     def menu_search_for_tools(self):
         print("This is the search menu")
@@ -271,22 +265,6 @@ class guiInterface:
 
     def menu_view_next_invoice(self):
         print("This is the menu to view the next invoice")
-
-    def register_menu_and_process(self):
-        self.user_account.register(
-            UserClass.generate_unique_ID(),
-            input("Please enter your first name: "),
-            input("Please enter your surname: "),
-            self.ask_valid_date_of_birth(),
-            self.ask_phone_number(),
-            input("Please enter your home address: "),
-            input("Please enter your post code: "),
-            self.ask_valid_email_address(),
-            self.get_hashed_password(),
-            0,                                           # Outstanding Balance
-            self.ask_user_type()
-        )
-        self.log_in_gui()
 
     def register_tool(self):
         # new_tool
@@ -312,81 +290,28 @@ class guiInterface:
     def goto_register_user_menu(self, event):
         self.register_user_gui()
 
-    def print_select_your_option(self):
-        print("Select your option: ", end=" ")
-
-    def get_valid_integer(self):
-        while(True):
-            try:
-                __consoleInput = int(input())
-                break
-            except:
-                print("Please enter an integer number.")
-        return __consoleInput
-
-    def get_valid_option(self, __x, __y):
-        while(True):
-            option1 = self.get_valid_integer()
-            if(__x <= option1 <= __y):
-                break
-        return option1
-
-    def get_hashed_password(self):
+    def get_valid_hashed_password(self):
         __password = self.password_input.get()
-        if 8 <= len(__password) <= 32:
-            __hashed_password = UserClass.generate_hashed_password(__password)
+        if(__password == self.confirm_password_input.get()):
+            if 8 <= len(__password) <= 32:
+                __hashed_password = UserClass.generate_hashed_password(
+                    __password)
+            else:
+                self.textvar.set("Please enter a password w/ 8 - 32 digits")
+                __hashed_password = None
         else:
-            self.textvar.set("Please enter a password w/ 8 - 32 digits")
-            __hashed_password = None
+            self.textvar.set("ERROR - Passwords do not match")
         return __hashed_password
 
-    def get_valid_price(self, __text: str):
-        __price = 0
-        while(True):
-            try:
-                __price = int(input(__text)) * 100
-                break
-            except:
-                print("Please enter correct price format e.g.: £20.00")
-        return __price
+    def get_valid_email_address(self):
+        __email = self.email_input.get()
+        if self.user_account.does_email_exist(__email):
+            self.textvar.set("ERROR - Email Already Exists")
+            __email = None
+        return __email
 
     def get_unix_timestamp(self):
         return int(time.time())
-
-    def ask_valid_date_of_birth(self):
-        try:
-            print("Please enter your date of birth:")
-            print("Day(1 - 31): ", end=" ")
-            __day = self.get_valid_option(1, 31)
-            print("Month(1 - 12): ", end=" ")
-            __month = self.get_valid_option(1, 12)
-            print(f"Year(1910 - {str(datetime.date.today().year)}): ", end=" ")
-            __year = self.get_valid_option(1900, datetime.date.today().year)
-            __date_of_birth = datetime.datetime(__year, __month, __day)
-            date_of_birth_str = __date_of_birth.strftime("%D")
-        except:
-            print("Error - The date entered is invalid")
-            date_of_birth_str = self.ask_valid_date_of_birth()
-        return date_of_birth_str
-
-    def ask_phone_number(self):
-        print("Please enter your phone number: ", end=" ")
-        return self.get_valid_integer()
-
-    def ask_valid_email_address(self):
-        while(True):
-            __email_address = input("Please enter your email address: ")
-            if(__email_address.find("@") != -1):
-                break
-        return __email_address
-
-    def ask_valid_email_address_not_in_DB(self):
-        while(True):
-            __email_address = self.ask_valid_email_address()
-            if not self.user_account.does_email_exist(__email_address):
-                break
-            else:
-                print("Email already taken please choose a different one.")
 
     def ask_user_type(self):
         print("As what type of user do you want to register?")
@@ -400,13 +325,10 @@ class guiInterface:
         }[__option]
         return __user_type
 
-    def ask_user_password(self):
-        __password = input("Please enter your password: ")
-        return __password
-
-    def escape(self):
-        # DO Nothing, useful for the workaround "switch statements"
-        pass
+    def get_buffered_user_errors(self):
+        buffered_errors = tuple(self.buffered_user_errors)
+        self.buffered_user_errors.clear()
+        return buffered_errors
 
     ########################################################    Main Program    ###########################################
 
